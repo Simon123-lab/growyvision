a2c6b0f8fc61c82651cc1a21649d5be4625e2d1e
Simon123-lab <63109996+Simon123-lab@users.noreply.github.com>
2022-01-04 14:09:43 +0500

    Update urls.py

--- growy/urls.py
+++ growy/urls.py
@@ -22,7 +22,7 @@ from django.views.static import serve
 urlpatterns = [
     path('admin/', admin.site.urls),
     path('',include('service.urls')),
-    url(r'^media/(?P<path>.*)$', serve,{'document_root': settings.MEDIA_ROOT}),
-    url(r'^static/(?P<path>.*)$', serve,{'document_root': settings.STATIC_ROOT}),
+    *(r'^media/(?P<path>.*)$', serve,{'document_root': settings.MEDIA_ROOT}),
+    *(r'^static/(?P<path>.*)$', serve,{'document_root': settings.STATIC_ROOT}),
     ]
 
